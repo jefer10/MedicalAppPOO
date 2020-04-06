d315da90e726512c182b12568d0b453c32e3ee77
jefer <jefersson290@hotmail.com>
2020-04-06 00:06:42 -0500

    se inicio el repositorio en local y la aplicacion

---
+++ src/Doctor.java
@@ -0,0 +1,13 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    /**
+     * Descripcion: la funcion solo imprime el nombre del doctor
+     */
+    public void  showName(){
+        System.out.println(name);
+    }
+}
